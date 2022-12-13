e3f05733080fcc760421d7816354a454ea30600a
valeriaradv <vradishevskaya@gmail.com>
2022-12-13 23:05:17 +0300

    Lesson21

---
+++ Lesson21/src/main/java/com/tms/Main.java
@@ -0,0 +1,8 @@
+package com.tms;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Git commit");
+    }
+}
